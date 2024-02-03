4817ebf7f7bc53c102843fe8542e54e7f3c3b8bd
Joe-D-Fer <97860159+Joe-D-Fer@users.noreply.github.com>
2024-02-03 10:04:21 -0300

    Update +server.ts

--- src/routes/return/+server.ts
+++ src/routes/return/+server.ts
@@ -1,6 +1,6 @@
 import { json } from "@sveltejs/kit";
 
-export async function POST({ request }) {
+export async function GET({ request }) {
 	const { token_ws } = await request.json();
 	console.log("token_ws:", token_ws);
 
